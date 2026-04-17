8a7af27d6893d782d84fe0850a6313762c62cd58
Linashihova <alexshashin.2023@gmail.com>
2026-04-17 18:10:17 +0500

    азработанная модель, контекст и адаптер данных для категории. Шашин

---
+++ app/src/main/java/com/example/recycler_view_shashin/Category.java
@@ -0,0 +1,11 @@
+package com.example.recycler_view_shashin;
+
+public class Category {
+    public int Id;
+    public String Name;
+    public Boolean Active = false;
+    public Category(int id, String name) {
+        this.Id = id;
+        this.Name = name;
+    }
+}
